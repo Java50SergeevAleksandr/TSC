e078ac40a861e789ae663d3c0d1a1e11dad5f3bf
AlGuron <blizgur8@gmail.com>
2023-03-02 19:32:07 +0200

    private methods

--- src/CipherDecipher.ts
+++ src/CipherDecipher.ts
@@ -8,33 +8,32 @@ export class CipherImpl implements Cipher {
 
     protected constructor(private shift: number, private shiftDirection: "left" | "right") { }
     cipher(plainText: string): string {
-        const mapperCipher: Function = this.shiftDirection == "left" ? getFunctionLeftShift() : getFunctionRightShift();
-        return shiftFromMapper(plainText, mapperCipher, this.shift);
+        const mapperCipher: Function = this.shiftDirection == "left" ? this.getFunctionLeftShift() : this.getFunctionRightShift();
+        return this.shiftFromMapper(plainText, mapperCipher, this.shift);
     }
     decipher(cipherText: string): string {
-        const mapperDecipher: Function = this.shiftDirection == "left" ? getFunctionRightShift() : getFunctionLeftShift();
-        return shiftFromMapper(cipherText, mapperDecipher, this.shift);
+        const mapperDecipher: Function = this.shiftDirection == "left" ? this.getFunctionRightShift() : this.getFunctionLeftShift();
+        return this.shiftFromMapper(cipherText, mapperDecipher, this.shift);
     }
-}
-
-function checkLetter(letter: string): boolean {
-    return (FIRST_SYMBOL <= letter.charCodeAt(0) && letter.charCodeAt(0) <= LAST_SYMBOL) ? true : false;
-}
-function getFunctionRightShift() {
-    return (symb: string, shift: number): string => {
-        const actualShift: number =
-            (symb.charCodeAt(0) - FIRST_SYMBOL + shift) % RANGE;
-        return String.fromCharCode(FIRST_SYMBOL + actualShift);
+    private getFunctionLeftShift() {
+        return (symb: string, shift: number): string => {
+            const actualShift: number = (LAST_SYMBOL - symb.charCodeAt(0) + shift) % RANGE;
+            return String.fromCharCode(LAST_SYMBOL - actualShift);
+        }
     }
-}
-function getFunctionLeftShift() {
-    return (symb: string, shift: number): string => {
-        const actualShift: number = (LAST_SYMBOL - symb.charCodeAt(0) + shift) % RANGE;
-        return String.fromCharCode(LAST_SYMBOL - actualShift);
+    private getFunctionRightShift() {
+        return (symb: string, shift: number): string => {
+            const actualShift: number =
+                (symb.charCodeAt(0) - FIRST_SYMBOL + shift) % RANGE;
+            return String.fromCharCode(FIRST_SYMBOL + actualShift);
+        }
     }
-}
-function shiftFromMapper(str: string, mapper: Function, shift: number) {
-    return Array.from(str).map(symb => {
-        return checkLetter(symb) ? mapper(symb, shift) : symb;
-    }).join('');
+    private shiftFromMapper(str: string, mapper: Function, shift: number) {
+        return Array.from(str).map(symb => {
+            return this.checkLetter(symb) ? mapper(symb, shift) : symb;
+        }).join('');
+    }
+    private checkLetter(letter: string): boolean {
+        return (FIRST_SYMBOL <= letter.charCodeAt(0) && letter.charCodeAt(0) <= LAST_SYMBOL) ? true : false;
+    }
 }
